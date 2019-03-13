d6ba8340eeec0962538f58d8931521374e294df8
Elena <kroska@ng.ru>
2019-03-14 00:27:54 +0700

    урок 1

--- src/lesson1/Cat.java
+++
@@ -1,8 +0,0 @@
-package lesson1;
-
-public class Cat extends  Animal{
-
-    public Cat(String type, String name) {
-        super(type, name, 200, 8, 1);
-    }
-}
--- src/lesson1/Obstacle.java
+++
@@ -1,7 +0,0 @@
-package lesson1;
-
-
-
-public abstract class Obstacle {
-    public abstract void doIt(Competitor competitor);
-}
